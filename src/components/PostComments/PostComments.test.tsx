e701af5b4f2f272056df108234af13d9a9962d17
David Marlon dos Santos <david.marlon94@gmail.com>
2024-12-14 10:34:15 +0000

    teste de inserção de comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -7,4 +7,25 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('inserção de dois comentários', () => {
+        const { debug } = render(<PostComment/>)
+        
+        fireEvent.change(screen.getByTestId('text'), {
+            target: {
+                value: 'Comentario de teste 1',
+            }
+        })
+        fireEvent.click(screen.getByTestId('button'))
+        
+        fireEvent.change(screen.getByTestId('text'), {
+            target: {
+                value: 'Comentario de teste 2',
+            }
+        })
+        fireEvent.click(screen.getByTestId('button'))
+        
+        expect(screen.getAllByTestId('comentario')).toHaveLength(2)
+        debug()
+    })
 });
